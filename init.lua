90dbd00157398f1492bd01d83e9c3ee05275c3aa
신동규 <donggyu9410@gmail.com>
2025-10-15 21:52:05 +0900

    apply everest

--- init.lua
+++ init.lua
@@ -27,135 +27,140 @@ vim.opt.cursorline = true
 -- lazy.nvim 부트스트랩
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable",
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- 플러그인 설정
 require("lazy").setup({
-  -- 컬러 스킴
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    config = function()
-      vim.cmd.colorscheme("catppuccin-mocha")
-    end,
-  },
-
-  -- Treesitter (구문 강조)
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-      require("nvim-treesitter.configs").setup({
-        ensure_installed = { "python", "lua", "vim", "vimdoc", "swift" },
-        auto_install = true,
-        highlight = { enable = true },
-        indent = { enable = true },
-      })
-    end,
-  },
-
-  -- LSP 설정
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      "williamboman/mason.nvim",
-      "williamboman/mason-lspconfig.nvim",
-      "j-hui/fidget.nvim",
+    -- 컬러 스킴
+    {
+        "sainnhe/everforest",
+        name = "everforest",
+        priority = 1000,
+        config = function()
+            -- Everforest 설정
+            vim.g.everforest_background = 'hard' -- 'hard', 'medium', 'soft'
+            vim.g.everforest_better_performance = 1
+            vim.g.everforest_enable_italic = 1
+            vim.g.everforest_dim_inactive_windows = 1
+            vim.cmd.colorscheme("everforest")
+        end,
     },
-  },
 
-  -- 자동완성
-  {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-path",
-      "L3MON4D3/LuaSnip",
-      "saadparwaiz1/cmp_luasnip",
+    -- Treesitter (구문 강조)
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        config = function()
+            require("nvim-treesitter.configs").setup({
+                ensure_installed = { "python", "lua", "vim", "vimdoc", "swift" },
+                auto_install = true,
+                highlight = { enable = true },
+                indent = { enable = true },
+            })
+        end,
     },
-  },
 
-  -- Formatter & Linter
-  {
-    "stevearc/conform.nvim",
-    event = { "BufWritePre" },
-    cmd = { "ConformInfo" },
-  },
-
-  -- 파일 탐색기
-  {
-    "nvim-tree/nvim-tree.lua",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-  },
-
-  -- 퍼지 파인더
-  {
-    "nvim-telescope/telescope.nvim",
-    branch = "0.1.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      {
-        "nvim-telescope/telescope-fzf-native.nvim",
-        build = "make"
-      }
-    },
-  },
-
-  -- 상태바
-  {
-    "nvim-lualine/lualine.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-  },
-
-  -- Git 통합
-  { "lewis6991/gitsigns.nvim" },
-
-  -- 주석
-  { "numToStr/Comment.nvim" },
-
-  -- 자동 페어
-  {
-    "windwp/nvim-autopairs",
-    event = "InsertEnter",
-  },
-
-  -- 인덴트 가이드
-  { "lukas-reineke/indent-blankline.nvim", main = "ibl" },
-
-  -- xcode-build-server (iOS/Xcode 프로젝트 지원)
-  {
-    "wojciech-kulik/xcodebuild.nvim",
-    dependencies = {
-      "nvim-telescope/telescope.nvim",
-      "MunifTanjim/nui.nvim",
-    },
-    config = function()
-      require("xcodebuild").setup({
-        -- xcode-build-server 자동 실행
-        code_coverage = {
-          enabled = true,
+    -- LSP 설정
+    {
+        "neovim/nvim-lspconfig",
+        dependencies = {
+            "williamboman/mason.nvim",
+            "williamboman/mason-lspconfig.nvim",
+            "j-hui/fidget.nvim",
         },
-      })
-    end,
-  },
+    },
+
+    -- 자동완성
+    {
+        "hrsh7th/nvim-cmp",
+        dependencies = {
+            "hrsh7th/cmp-nvim-lsp",
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-path",
+            "L3MON4D3/LuaSnip",
+            "saadparwaiz1/cmp_luasnip",
+        },
+    },
+
+    -- Formatter & Linter
+    {
+        "stevearc/conform.nvim",
+        event = { "BufWritePre" },
+        cmd = { "ConformInfo" },
+    },
+
+    -- 파일 탐색기
+    {
+        "nvim-tree/nvim-tree.lua",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+    },
+
+    -- 퍼지 파인더
+    {
+        "nvim-telescope/telescope.nvim",
+        branch = "0.1.x",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            {
+                "nvim-telescope/telescope-fzf-native.nvim",
+                build = "make"
+            }
+        },
+    },
+
+    -- 상태바
+    {
+        "nvim-lualine/lualine.nvim",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+    },
+
+    -- Git 통합
+    { "lewis6991/gitsigns.nvim" },
+
+    -- 주석
+    { "numToStr/Comment.nvim" },
+
+    -- 자동 페어
+    {
+        "windwp/nvim-autopairs",
+        event = "InsertEnter",
+    },
+
+    -- 인덴트 가이드
+    { "lukas-reineke/indent-blankline.nvim", main = "ibl" },
+
+    -- xcode-build-server (iOS/Xcode 프로젝트 지원)
+    {
+        "wojciech-kulik/xcodebuild.nvim",
+        dependencies = {
+            "nvim-telescope/telescope.nvim",
+            "MunifTanjim/nui.nvim",
+        },
+        config = function()
+            require("xcodebuild").setup({
+                -- xcode-build-server 자동 실행
+                code_coverage = {
+                    enabled = true,
+                },
+            })
+        end,
+    },
 })
 
 -- Mason 설정 (LSP 서버 관리)
 require("mason").setup()
 require("mason-lspconfig").setup({
-  ensure_installed = { "pyright", "ruff" },
-  automatic_installation = true,
+    ensure_installed = { "pyright", "ruff" },
+    automatic_installation = true,
 })
 
 -- LSP 설정
@@ -163,81 +168,81 @@ local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 -- Pyright (Python LSP)
 vim.lsp.config.pyright = {
-  cmd = { "pyright-langserver", "--stdio" },
-  filetypes = { "python" },
-  root_markers = { "pyproject.toml", "setup.py", "setup.cfg", "requirements.txt", "Pipfile", "pyrightconfig.json" },
-  capabilities = capabilities,
-  settings = {
-    python = {
-      analysis = {
-        autoSearchPaths = true,
-        diagnosticMode = "workspace",
-        useLibraryCodeForTypes = true,
-      },
+    cmd = { "pyright-langserver", "--stdio" },
+    filetypes = { "python" },
+    root_markers = { "pyproject.toml", "setup.py", "setup.cfg", "requirements.txt", "Pipfile", "pyrightconfig.json" },
+    capabilities = capabilities,
+    settings = {
+        python = {
+            analysis = {
+                autoSearchPaths = true,
+                diagnosticMode = "workspace",
+                useLibraryCodeForTypes = true,
+            },
+        },
     },
-  },
 }
 
 -- Ruff (Python Linter)
 vim.lsp.config.ruff = {
-  cmd = { "ruff", "server" },
-  filetypes = { "python" },
-  root_markers = { "pyproject.toml", "setup.py", "setup.cfg", "requirements.txt", "Pipfile", "ruff.toml" },
-  capabilities = capabilities,
+    cmd = { "ruff", "server" },
+    filetypes = { "python" },
+    root_markers = { "pyproject.toml", "setup.py", "setup.cfg", "requirements.txt", "Pipfile", "ruff.toml" },
+    capabilities = capabilities,
 }
 
 -- SourceKit (Swift LSP)
 vim.lsp.config.sourcekit = {
-  cmd = { "sourcekit-lsp" },
-  filetypes = { "swift", "objc", "objcpp" },
-  root_markers = { "Package.swift", ".git", "*.xcodeproj", "*.xcworkspace" },
-  capabilities = capabilities,
-  settings = {
-    sourcekit = {
-      -- Xcode 프로젝트 지원 강화
-      indexing = {
-        enabled = true,
-      },
+    cmd = { "sourcekit-lsp" },
+    filetypes = { "swift", "objc", "objcpp" },
+    root_markers = { "Package.swift", ".git", "*.xcodeproj", "*.xcworkspace" },
+    capabilities = capabilities,
+    settings = {
+        sourcekit = {
+            -- Xcode 프로젝트 지원 강화
+            indexing = {
+                enabled = true,
+            },
+        },
     },
-  },
-  on_attach = function(client, bufnr)
-    -- iOS 프로젝트의 경우 인덱싱이 완료될 때까지 대기
-    vim.defer_fn(function()
-      vim.notify("SourceKit LSP ready for " .. vim.fn.expand("%:t"), vim.log.levels.INFO)
-    end, 2000)
-  end,
+    on_attach = function(client, bufnr)
+        -- iOS 프로젝트의 경우 인덱싱이 완료될 때까지 대기
+        vim.defer_fn(function()
+            vim.notify("SourceKit LSP ready for " .. vim.fn.expand("%:t"), vim.log.levels.INFO)
+        end, 2000)
+    end,
 }
 
 -- LSP 자동 시작
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = "python",
-  callback = function()
-    vim.lsp.enable({ "pyright", "ruff" })
-  end,
+    pattern = "python",
+    callback = function()
+        vim.lsp.enable({ "pyright", "ruff" })
+    end,
 })
 
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "swift", "objc", "objcpp" },
-  callback = function()
-    vim.lsp.enable("sourcekit")
-  end,
+    pattern = { "swift", "objc", "objcpp" },
+    callback = function()
+        vim.lsp.enable("sourcekit")
+    end,
 })
 
 -- LSP 키 매핑
 vim.api.nvim_create_autocmd("LspAttach", {
-  group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-  callback = function(ev)
-    local opts = { buffer = ev.buf, silent = true }
-    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-    vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
-    vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-    vim.keymap.set("n", "<leader>f", function()
-      vim.lsp.buf.format({ async = true })
-    end, opts)
-  end,
+    group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+    callback = function(ev)
+        local opts = { buffer = ev.buf, silent = true }
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+        vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+        vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+        vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+        vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+        vim.keymap.set("n", "<leader>f", function()
+            vim.lsp.buf.format({ async = true })
+        end, opts)
+    end,
 })
 
 -- nvim-cmp 자동완성 설정
@@ -245,77 +250,77 @@ local cmp = require("cmp")
 local luasnip = require("luasnip")
 
 cmp.setup({
-  snippet = {
-    expand = function(args)
-      luasnip.lsp_expand(args.body)
-    end,
-  },
-  mapping = cmp.mapping.preset.insert({
-    ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-    ["<C-f>"] = cmp.mapping.scroll_docs(4),
-    ["<C-Space>"] = cmp.mapping.complete(),
-    ["<CR>"] = cmp.mapping.confirm({ select = true }),
-    ["<Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
-    ["<S-Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
-  }),
-  sources = {
-    { name = "nvim_lsp" },
-    { name = "luasnip" },
-    { name = "buffer" },
-    { name = "path" },
-  },
+    snippet = {
+        expand = function(args)
+            luasnip.lsp_expand(args.body)
+        end,
+    },
+    mapping = cmp.mapping.preset.insert({
+        ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+                luasnip.expand_or_jump()
+            else
+                fallback()
+            end
+        end, { "i", "s" }),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+                luasnip.jump(-1)
+            else
+                fallback()
+            end
+        end, { "i", "s" }),
+    }),
+    sources = {
+        { name = "nvim_lsp" },
+        { name = "luasnip" },
+        { name = "buffer" },
+        { name = "path" },
+    },
 })
 
 -- Conform (Formatter) 설정
 require("conform").setup({
-  formatters_by_ft = {
-    python = { "black", "isort" },
-    swift = { "swiftformat" },
-  },
-  format_on_save = {
-    timeout_ms = 500,
-    lsp_fallback = true,
-  },
+    formatters_by_ft = {
+        python = { "black", "isort" },
+        swift = { "swiftformat" },
+    },
+    format_on_save = {
+        timeout_ms = 500,
+        lsp_fallback = true,
+    },
 })
 
 -- nvim-tree 설정
 require("nvim-tree").setup({
-  view = {
-    width = 30,
-  },
-  update_focused_file = {
-    enable = true,      -- 파일 변경 시 자동으로 트리에서 해당 파일 위치로 이동
-    update_cwd = false, -- 작업 디렉토리는 변경하지 않음
-  },
+    view = {
+        width = 30,
+    },
+    update_focused_file = {
+        enable = true,      -- 파일 변경 시 자동으로 트리에서 해당 파일 위치로 이동
+        update_cwd = false, -- 작업 디렉토리는 변경하지 않음
+    },
 })
 
 -- Telescope 설정
 local telescope = require("telescope")
 telescope.setup({
-  defaults = {
-    mappings = {
-      i = {
-        ["<C-u>"] = false,
-        ["<C-d>"] = false,
-      },
+    defaults = {
+        mappings = {
+            i = {
+                ["<C-u>"] = false,
+                ["<C-d>"] = false,
+            },
+        },
     },
-  },
 })
 
 -- fzf 확장 로드
@@ -323,11 +328,11 @@ pcall(require("telescope").load_extension, "fzf")
 
 -- Lualine 설정
 require("lualine").setup({
-  options = {
-    theme = "catppuccin",
-    component_separators = "|",
-    section_separators = "",
-  },
+    options = {
+        theme = "everforest",
+        component_separators = "|",
+        section_separators = "",
+    },
 })
 
 -- Gitsigns 설정
